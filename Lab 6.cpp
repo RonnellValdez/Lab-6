3852dd5bfe1442ff5e3667527987ebdbee16f504
RonnellValdez <ronnell.b.valdez@gmail.com>
2020-05-26 13:36:28 -0700

    modifications to the cpp file

--- Lab 6.cpp
+++ Lab 6.cpp
@@ -7,17 +7,23 @@ using namespace std;
 
 int main()
 {
-	cout << setiosflags(ios_base::fixed);									//Do Not Use E Notation
-	cout << setiosflags(ios_base::showpoint);								//Always Show Decimal Point
-	cout << setprecision(2);												//Rounded To 2 Decimal Places
+	cout << setiosflags(ios_base::fixed);										//Do Not Use E Notation
+	cout << setiosflags(ios_base::showpoint);										//Always Show Decimal Point
+	cout << setprecision(2);										//Rounded To 2 Decimal Places
 
 	int Weeks;
-	double HoursWeek, TotalHoursWeek, WeekPay, TotalPay;
+	double hoursWeek, totalHoursweek, weekPay, totalPay;
 
-	TotalPay = 0;														//Initialization For TotalPay
+	totalPay = 0;										//Initialization For TotalPay
+	const float Salary = 7.72;									//Const for rate of pay per hour
+	const float taxReductions = .33;										//Const for tax reductions from paycheck
+	
+	
+	
+	
 	cout << "How many weeks in this pay period? ";
 	cin >> Weeks;
-	if (Weeks <= 0) {
+	if (Weeks <= 0) {										//Edgecase For Weeks
 		cout << "Error: Must be at least 1 week" << endl;
 		return 0;
 	}
@@ -25,18 +31,18 @@ int main()
 	
 	while (Weeks > 0) {
 		cout << "How many hours did you work this week? ";
-		cin >> HoursWeek;
-		if (HoursWeek <= 0) {
+		cin >> hoursWeek;
+		if (hoursWeek < 0) {											//Edgecase For HoursWeek
 			cout << "Error: Must be at least 1 hour" << endl;
 			return 0;
 		}
-		TotalHoursWeek = (HoursWeek*7.72);
-		WeekPay = TotalHoursWeek - (TotalHoursWeek*.33);
-		cout << "Your pay for this week is $" <<  WeekPay << endl;
-		TotalPay = WeekPay + TotalPay;
+		totalHoursweek = (hoursWeek*Salary);
+		weekPay = totalHoursweek - (totalHoursweek*taxReductions);
+		cout << "Your pay for this week is $" <<  weekPay << endl;
+		totalPay = weekPay + totalPay;
 		Weeks--;
 	}
-	cout << "Your total pay for this pay period is $" << TotalPay;
+	cout << "Your total pay for this pay period is $" << totalPay;
 
 	
 	return 0;
