74d6a89e737afa4c2a796b351a80e507c0e1d52b
RonnellValdez <44014205+RonnellValdez@users.noreply.github.com>
2020-05-26 13:42:00 -0700

    Update Lab 6.cpp

--- Lab 6.cpp
+++ Lab 6.cpp
@@ -9,21 +9,21 @@ int main()
 {
 	cout << setiosflags(ios_base::fixed);									//Do Not Use E Notation
 	cout << setiosflags(ios_base::showpoint);								//Always Show Decimal Point
-	cout << setprecision(2);												//Rounded To 2 Decimal Places
+	cout << setprecision(2);										//Rounded To 2 Decimal Places
 
 	int Weeks;
 	double hoursWeek, totalHoursweek, weekPay, totalPay;
 
-	totalPay = 0;															//Initialization For TotalPay
-	const float Salary = 7.72;												//Const for rate of pay per hour
-	const float taxReductions = .33;										//Const for tax reductions from paycheck
+	totalPay = 0;												//Initialization For TotalPay
+	const float Salary = 7.72;										//Const for rate of pay per hour
+	const float taxReductions = .33;									//Const for tax reductions from paycheck
 	
 	
 	
 	
 	cout << "How many weeks in this pay period? ";
 	cin >> Weeks;
-	if (Weeks <= 0) {														//Edgecase For Weeks
+	if (Weeks <= 0) {											//Edgecase For Weeks
 		cout << "Error: Must be at least 1 week" << endl;
 		return 0;
 	}
@@ -32,7 +32,7 @@ int main()
 	while (Weeks > 0) {
 		cout << "How many hours did you work this week? ";
 		cin >> hoursWeek;
-		if (hoursWeek < 0) {												//Edgecase For HoursWeek
+		if (hoursWeek < 0) {										//Edgecase For HoursWeek
 			cout << "Error: Must be at least 1 hour" << endl;
 			return 0;
 		}
